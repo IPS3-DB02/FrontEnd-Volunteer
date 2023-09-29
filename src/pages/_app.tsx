55131d5c5e7a520552f7bac25b86639aa6381d05
JRaemakers <joostrae@hotmail.nl>
2023-09-29 09:33:37 +0200

    Use next/font for local fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { Roboto } from 'next/font/google'
+import localFont from 'next/font/local'
 
 const roboto = Roboto({
   weight: ['100', '300', '400', '500', '700', '900'],
   style: ['normal', 'italic'],
   subsets: ['latin'],
 })
+
+const brandish = localFont({ src: '../fonts/brandish.ttf' })
+const centuryGothic = localFont({ src: '../fonts/centurygothic.ttf' })
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
@@ -15,6 +19,8 @@ const App = ({ Component, pageProps }: AppProps) => {
         {`
           :root {
             --font-base: ${roboto.style.fontFamily};
+            --font-brandish: ${brandish.style.fontFamily};
+            --font-century-gothic: ${centuryGothic.style.fontFamily};
           }
         `}
       </style>
