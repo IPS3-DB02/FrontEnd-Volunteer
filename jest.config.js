6ec9c0b368e28023a5fc282137d6dcc1f5ccd4b6
1Sjoerd <sjoerd@skindustries.nl>
2023-11-08 12:42:17 +0100

    Jest Implementation

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        babel: true,
+        tsconfig: 'tsconfig.test.json',
+      },
+    ],
+  },
+}
